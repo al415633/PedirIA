d1f62e44868410a2b1a1a6849c655c6628677b2c
MCY-1911 <al415647@uji.es>
2025-02-25 16:21:41 +0100

    feat(sp1|hu11): DAOs de Carne, Verduras y Frutas

---
+++ src/main/java/services/CarneDAOJPA.java
@@ -0,0 +1,56 @@
+package services;
+
+import data.Carne;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@ApplicationScoped
+public class CarneDAOJPA {
+    @Inject
+    EntityManager em;
+
+    @Transactional
+    public Carne create(Carne carne) {
+        if (em.find(Carne.class, carne.getId()) != null) {
+            return null;
+        }
+        em.persist(carne);
+        return carne;
+    }
+
+    public Carne retrieve(Long id) {
+        return Objects.requireNonNullElse(em.find(Carne.class, id), null);
+    }
+
+    @Transactional
+    public Carne update(Carne carne) {
+        if (em.find(Carne.class, carne.getId()) == null) {
+            return null;
+        }
+        return em.merge(carne);
+    }
+
+    @Transactional
+    public Carne delete(Long id) {
+        Carne found = em.find(Carne.class, id);
+        if (found == null) {
+            return null;
+        }
+        em.remove(found);
+        return found;
+    }
+
+    public Collection<Carne> getAll() {
+        TypedQuery<Carne> query = em.createQuery("SELECT c FROM Carne c", Carne.class);
+        List<Carne> result = query.getResultList();
+        return result != null ? result : new ArrayList<>();
+    }
+}
